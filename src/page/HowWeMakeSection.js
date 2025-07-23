75f5ea5214c54112ebe9542eba4771fdd3a8fd15
SanjayChourasiya <sanjayintern01@gmail.com>
2025-07-23 23:25:08 +0530

    update changes on image name

--- src/page/HowWeMakeSection.js
+++ src/page/HowWeMakeSection.js
@@ -67,7 +67,7 @@ function SoftBeforeAfter({ before, after }) {
 export default function BeforeAfterGrid() {
   const portfolio = [
     { before: "/img/hb.jpg", after: "/img/ha.jpg" },
-    { before: "/img/sa.jpg", after: "/img/sb.png" },
+    { before: "/img/sb.png", after: "/img/sb.jpg" },
     { before: "/img/b.jpg", after: "/img/f.jpg" },
     { before: "/img/b.jpg", after: "/img/f.jpg" },
 
